6af2b3bcd074c12b50e59b6d3394f2afb92df2cb
Nishant Singh <117887760+Nishock@users.noreply.github.com>
2025-03-23 17:07:42 +0530

    read

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,14 +1,21 @@
 import admin from "firebase-admin";
 
+// Ensure that required environment variables exist
+if (
+  !process.env.FIREBASE_PROJECT_ID ||
+  !process.env.FIREBASE_PRIVATE_KEY ||
+  !process.env.FIREBASE_CLIENT_EMAIL
+) {
+  throw new Error("Missing one or more Firebase environment variables.");
+}
+
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
-        type: process.env.FIREBASE_TYPE,
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       }),
     });
     console.log("✅ Firebase Initialized Successfully!");
